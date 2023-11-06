0e5cbd86e86e1d00c4958e18fa759baa73226ff6
NikitaTorbenko <2015nekitciti@gmail.com>
2023-11-06 04:13:24 +0300

    feat: add adaptive to sidebar

--- src/components/menuScreens/Sidebar.tsx
+++ src/components/menuScreens/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../assets/scss/widgets/_menu.scss';
 import { ReactComponent as CloseSidebar } from '../../assets/icons/menu/close.svg';
+import { ReactComponent as Logo } from '../../assets/icons/sidebar/logo.svg';
 import { menuItemsType } from '../../types';
 
 interface SidebarProps {
@@ -22,7 +23,10 @@ export const Sidebar = ({
     >
       <div className='sidebarContainer'>
         <div className='sidebarTop'>
-          <CloseSidebar onClick={() => setIsActiveBurger(false)} />
+          <div className='sidebarTop-icons'>
+            <Logo className='sidebarTop-icons__logo' />
+            <CloseSidebar onClick={() => setIsActiveBurger(false)} />
+          </div>
           <div className='menu-navbar'>
             {menuItems.map((el, index) => (
               <div className='menu-navbar__item' key={index}>
@@ -37,6 +41,7 @@ export const Sidebar = ({
             <div className='menu-contact__number'>+7 (918) 432-65-87</div>
             <div className='menu-contact__time'>Ежедневно с 9:00 до 23:00</div>
           </div>
+          <div className='sidebarBottom-lang'>English</div>
         </div>
       </div>
     </div>
